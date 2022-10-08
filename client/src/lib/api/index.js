8312f3b902f14df576e3c072370782f065a02c56
js_lee <jongsuklee45@gmail.com>
2022-10-08 21:14:19 +0900

    fix: fix signup minor bug

--- client/src/lib/api/index.js
+++ client/src/lib/api/index.js
@@ -1,9 +1,9 @@
 import axiosInstance from 'axios';
 
 const axios = axiosInstance.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
   // baseURL: 'https://memint-server.herokuapp.com/',
-  // baseURL: 'https://memint-app-server.herokuapp.com/',
+  baseURL: 'https://memint-app-server.herokuapp.com/',
 });
 
 export default axios;
